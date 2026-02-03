e3123164f59e0b2ebbee66ab3a8eade6eef26764
Deepanshu Singh <deevyanshusingh123@gmail.com>
2026-02-03 12:23:44 +0530

    shema fir

--- schema.js
+++ schema.js
@@ -1,18 +1,19 @@
-const joi=require('joi');
-module.exports.listingSchema=joi.object({  
-    listing:joi.object({
-        title:joi.string().required(),
-    description:joi.string().required(),
-     price:joi.number().required().min(0),
-     location:joi.string().required(),
-     country:joi.string().required(),                   
-    image:joi.string().allow("",null),
-    }).required(),
+const Joi = require("joi");
 
-    });
-    module.exports.reviewSchema = joi.object({
-  review: joi.object({
-      rating: joi.number().required(),
-    comment: joi.string().required(),
+module.exports.listingSchema = Joi.object({
+  listing: Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+    // ❌ image yahan nahi aayegi
+  }).required(),
+});
+
+module.exports.reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().required(),
+    comment: Joi.string().required(),
   }).required(),
 });
