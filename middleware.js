21f91c0ec55266d3fae8463994b0b7cae42b8687
Deepanshu Singh <deevyanshusingh123@gmail.com>
2026-02-03 13:16:24 +0530

    log joi validation error

--- middleware.js
+++ middleware.js
@@ -32,16 +32,19 @@ module.exports.isOwner=async(req,res,next)=>{
 }
 
 
-
 module.exports.validateListing = (req, res, next) => {
+  console.log("VALIDATE LISTING BODY 👉", req.body);
+
   let { error } = listingSchema.validate(req.body);
+
   if (error) {
+    console.log("❌ JOI ERROR 👉", error.details);
     let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
+    return next(new ExpressError(400, errMsg));
   }
+  next();
 };
+
 
   
 module.exports. validateReview = (req, res, next) => {
